0df0e17643b71151073bf4618d3db2cdd80eb345
ehoi0 <61307271+ehoi0@users.noreply.github.com>
2020-05-14 17:37:37 +0900

    자바 문제 풀어야 될거1

---
+++ Sort.java
@@ -0,0 +1,76 @@
+package interfaceEx03_8;
+
+interface Sort {
+	void ascedning(int [] arr);
+	void descending(int [] arr);
+	void description();
+}
+class QuickSort implements Sort {
+
+	@Override
+	public void ascedning(int [] arr) {
+		
+		
+	}
+
+	@Override
+	public void descending(int [] arr) {
+		
+		
+	}
+
+	@Override
+	public void description() {
+		
+		
+	}
+}
+	class HeapSort implements Sort {
+
+		@Override
+		public void ascedning(int [] arr) {
+			
+			
+		}
+
+		@Override
+		public void descending(int [] arr) {
+			
+			
+		}
+
+		@Override
+		public void description() {
+			
+			
+		}
+	}
+		
+class BubbleSort implements Sort {
+
+			@Override
+			public void ascedning(int [] arr) {
+				
+				
+			}
+
+			@Override
+			public void descending(int [] arr) {
+				
+				
+			}
+
+			@Override
+			public void description() {
+				
+				
+			}
+
+}
+
+
+
+
+
+	
+
